614e84c1fac5be983a8aa2ce1a3d4545a0402cc3
zoucg <mr_zoucg@sina.com>
2018-11-14 21:04:39 +0800

    add check_results files, check the labeled image

--- check_results.py
+++ check_results.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os
+import time
 
 class ResultsManger(object):
     def __init__(self, source_img_dir, source_labe_dir):
@@ -10,15 +11,15 @@ class ResultsManger(object):
     def prase_result_file(self, file_path):
         with open(file_path, 'r') as f:
             objectlist = f.readlines()
-            print(objectlist)
+            print('objectlist',objectlist)
             if len(objectlist)==0:
                 return
 
-            objectlist = objectlist[:-1]
             points_list = []
             for object in objectlist:
                 object = object.split(' ')
-                print(object)
+                if len(object)!= 11:
+                    continue
                 p0 = [int(object[1]), int(object[2])]
                 p1 = [int(object[3]), int(object[4])]
                 p2 = [int(object[5]), int(object[6])]
@@ -39,29 +40,43 @@ class ResultsManger(object):
         return img
 
     def get_out_results(self, dest_dir):
+        t = time.localtime()
+        data = t.tm_mday
+        hour = t.tm_hour
+        min = t.tm_min
+        logpath = '/home/zoucg/new_hhd/log/log_{}_{}_{}.txt'.format(data, hour, min)
+        f = open(logpath, 'a')
         if os.path.exists(dest_dir) is False:
             os.makedirs(dest_dir)
         labels = os.listdir(self.source_label_dir)
         labels = (label for label in labels if label.endswith('txt'))
 
         for i, label in enumerate(labels):
+            if i<=1000:
+                continue
             print(label)
             label_path = os.path.join(self.source_label_dir, label)
             points_list = self.prase_result_file(label_path)
+            if points_list is None:
+                print(i, label_path, file=f)
+                continue
             img_name = label[:-3] + 'png'
             img_path = os.path.join(self.source_img_dir, img_name)
             img = self.draw_object(points_list, img_path)
             img_dest_path = os.path.join(dest_dir, img_name)
             cv2.imwrite(img_dest_path, img)
-            if i > 10:
+
+            print(i, img_path, file=f)
+
+            if i >10000:
                 break
 
 
 def main():
-    source_img_dir = '/home/zoucg/new_hhd/LABELED_SHIP/part0'
-    source_label_dir = '/home/zoucg/new_hhd/LABELED_SHIP/part0'
+    source_img_dir = '/media/zoucg/Maxtor/8'
+    source_label_dir = '/media/zoucg/Maxtor/8'
     label_result = ResultsManger(source_img_dir, source_label_dir)
-    label_result.get_out_results('/home/zoucg/new_hhd/label_check')
+    label_result.get_out_results('/home/zoucg/new_hhd/label_check4')
 
 if __name__=='__main__':
     main()
